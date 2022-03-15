5e329ef629d001cdaab9a862e6e4bef114cead05
luizfelipetorres <fellipe116@gmail.com>
2022-03-15 15:46:01 -0300

    Adicionado o menu lateral. Ainda não configurado

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <template>
   <v-app>
-    <v-app-bar
-      color="deep-purple accent-4"
-      dense
-      dark
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Campeonato Brasileiro </v-toolbar-title>
-
+    <v-app-bar color="green lighten-1" dense dark app>
+      <v-app-bar-nav-icon  @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>Campeonato Brasileiro ⚽</v-toolbar-title>
     </v-app-bar>
+
+  <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-main>
     </v-main>
@@ -25,7 +49,8 @@ export default {
   },
 
   data: () => ({
-    //
+    drawer: false,
+    group: null,
   }),
 };
 </script>
