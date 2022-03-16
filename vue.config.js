911971f0bb973b8f2c1fb842d0269e867b361a78
luizfelipetorres <fellipe116@gmail.com>
2022-03-16 12:59:45 -0300

    Teste pages 2

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = {
-  publicPath: '/vue-campeonato-brasileiro/'
-}
-
-//process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/"
+module.exports = defineConfig({
+  transpileDependencies: [
+    'vuetify'
+  ]
+})
