769b7efef4ebd17fea1bc9b3763cf1bb9a876d87
luizfelipetorres <fellipe116@gmail.com>
2022-03-16 20:13:35 -0300

    Tabela de classificação movida para componente

---
+++ src/components/TabelaClubes.vue
@@ -0,0 +1,65 @@
+<template>
+        <v-simple-table>
+            <template v-slot:default>
+            <thead>
+                
+                    <tr>         
+                        <th class="text-center ">
+                            Colocaçãa
+                        </th>
+                        <th class="text-center">
+                            Clube
+                        </th>
+                        <th class="text-center">
+                            Pontos       
+                        </th>
+                    </tr>
+
+                <tr v-for="(clube, index) of clubesListaOrdenada" v-bind:key="clube.id">
+                    <td class="text-center"> {{index + 1}} </td>
+                    <td class="text-left"> 
+                    <v-avatar size="24">
+                            <img :src="clube.escudo" :alt="clube.nome">
+                        </v-avatar>
+                        <span class="pl-3   ">{{clube.nome}} </span>
+                    </td>
+                    <td class="text-center"> {{clube.pontos}} </td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+            </template>
+        </v-simple-table>    
+</template>
+
+<script>
+export default {
+    name: 'TabelaClubes',
+    data(){
+        return {
+            clubesLista: []
+        }
+    },
+    created(){
+        fetch('https://hackthon-decola.firebaseio.com/clubes-lista.json')
+            .then(resposta => resposta.json())
+            .then(json => {
+                this.clubesLista = json
+                //console.log(this.clubesLista)
+            })
+    },
+    computed: {
+        clubesListaOrdenada(){
+            const listaOrdenada = this.clubesLista.slice(0).sort(
+                (a, b) =>   a.pontos > b.pontos ? -1 : 1 
+            )
+            return listaOrdenada    
+        }
+    }
+}
+</script>
+
